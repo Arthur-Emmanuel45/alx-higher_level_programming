e19721f7a0f83cc5180b08a720d668565281b7b5
Arthur-Emmanuel45 <emmanuelarthur45@gmail.com>
2023-08-10 15:12:05 -0700

    if_elif_else loop in python

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,5 @@ def uppercase(str):
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
             c = chr(ord(c) - 32)
-        else:
-            c = i
         print("{}".format(c), end="")
     print("")
